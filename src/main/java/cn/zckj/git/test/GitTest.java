30d5c4f355a4c30b5d53b917251cb2bac6c3ef6a
zckj <182657@qq.com>
2022-07-27 17:42:12 +0800

    second commit

--- src/main/java/cn/zckj/git/test/GitTest.java
+++ src/main/java/cn/zckj/git/test/GitTest.java
@@ -3,5 +3,6 @@ package cn.zckj.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello 2");
     }
 }
